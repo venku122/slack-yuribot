e7528cb56d46d33c96a86936f96946905e299c18
Sangwon Lee <gamzabaw@gmail.com>
2015-04-29 21:44:46 +0900

    test

--- handlers/out.js
+++ handlers/out.js
@@ -2,17 +2,17 @@ var BOT_NAME = 'slackbot';
 var TOKEN = '6J09yxyd5IpCNjzQfm5XOG8q';
 
 module.exports = function (req, res, next) {
+  var botPayload = {
+    text: userName
+  };
+  return res.status(200).json(botPayload);
+
   var token = req.body.token;
   if (token !== TOKEN) {
     return res.status(200).end();
   }
 
   var userName = req.body.user_name;
-  var botPayload = {
-    text: userName
-  };
-  return res.status(200).json(botPayload);
-
   if (userName === BOT_NAME) {
     return res.status(200).end();
   }
