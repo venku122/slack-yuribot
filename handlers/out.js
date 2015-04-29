4bc03a12c497fb535095a23ef3250c0dc32975ed
Sangwon Lee <gamzabaw@gmail.com>
2015-04-29 22:33:45 +0900

    incoming handler

--- handlers/out.js
+++ handlers/out.js
@@ -1,4 +1,4 @@
-var BOT_NAME = 'slackbot';
+var BOT_NAME = 'yesdoubt';
 var TOKEN = '80xdkjKkn02JgCic18Vmwe7M';
 
 module.exports = function (req, res, next) {
@@ -15,7 +15,7 @@ module.exports = function (req, res, next) {
   var text = req.body.text;
   if (text.indexOf("sexy") === -1) {
     return res.status(200).end();
-  } 
+  }
 
   var botPayload = {
     text: 'https://www.google.co.kr/search?q=sexy&newwindow=1&biw=1244&bih=682&source=lnms&tbm=isch&sa=X&ei=z81AVer4NYe4mwXFj4CgCw&ved=0CAYQ_AUoAQ'
