90f6f8ea29bdfe2e6f7fcd2e147103b15c9879e2
Sangwon Lee <gamzabaw@gmail.com>
2015-04-29 21:59:26 +0900

    nodoubt

--- handlers/out.js
+++ handlers/out.js
@@ -1,30 +1,24 @@
 var BOT_NAME = 'slackbot';
-var TOKEN = '50dHrPcZZjEGObSgW6RZITfE';
+var TOKEN = '80xdkjKkn02JgCic18Vmwe7M';
 
 module.exports = function (req, res, next) {
+  var token = req.body.token;
+  if (token !== TOKEN) {
+    return res.status(200).end();
+  }
+
   var userName = req.body.user_name;
+  if (userName === BOT_NAME) {
+    return res.status(200).end();
+  }
+
+  var text = req.body.text;
+  if (text.indexOf("sexy") === -1) {
+    return res.status(200).end();
+  } 
+
   var botPayload = {
-    text: userName
+    text: 'https://www.google.co.kr/search?q=sexy&newwindow=1&biw=1244&bih=682&source=lnms&tbm=isch&sa=X&ei=z81AVer4NYe4mwXFj4CgCw&ved=0CAYQ_AUoAQ'
   };
   return res.status(200).json(botPayload);
-
-  // var token = req.body.token;
-  // if (token !== TOKEN) {
-  //   return res.status(200).end();
-  // }
-
-  // var userName = req.body.user_name;
-  // if (userName === BOT_NAME) {
-  //   return res.status(200).end();
-  // }
-
-  // var text = req.body.text;
-  // if (text.indexOf("sexy") === -1) {
-  //   return res.status(200).end();
-  // } 
-
-  // var botPayload = {
-  //   text: 'https://www.google.co.kr/search?q=sexy&newwindow=1&biw=1244&bih=682&source=lnms&tbm=isch&sa=X&ei=z81AVer4NYe4mwXFj4CgCw&ved=0CAYQ_AUoAQ'
-  // };
-  // return res.status(200).json(botPayload);
 }
