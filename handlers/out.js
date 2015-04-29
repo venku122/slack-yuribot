72104f614372bcd66d304f0f6172c84cac87d240
Sangwon Lee <gamzabaw@gmail.com>
2015-04-29 21:42:34 +0900

    echo bot

--- handlers/out.js
+++ handlers/out.js
@@ -8,14 +8,14 @@ module.exports = function (req, res, next) {
   }
 
   var userName = req.body.user_name;
+  var botPayload = {
+    text: userName
+  };
+  return res.status(200).json(botPayload);
+
   if (userName === BOT_NAME) {
     return res.status(200).end();
   }
-
-  var botPayload = {
-    text: 'https://www.google.co.kr/search?q=sexy&newwindow=1&biw=1244&bih=682&source=lnms&tbm=isch&sa=X&ei=z81AVer4NYe4mwXFj4CgCw&ved=0CAYQ_AUoAQ'
-  };
-  return res.status(200).json(botPayload);
 
   // var text = req.body.text;
   // if (text.indexOf("sexy") === -1) {
