83619df85cb46766b16b471ca16f3096991f9b1f
Thomas Tarazevits <ttarazevits@hubspot.com>
2017-04-18 21:30:57 -0400

    ready to ship

--- handlers/slash/yuri.js
+++ handlers/slash/yuri.js
@@ -26,10 +26,14 @@ module.exports = function (req, res, next) {
 
   // send query to nasa api
   request(`${NASAUrl}/search?q=${query}&media_type=image`, (err, nasaRes, nasaBody) => {
-    var item = nasaBody.collection.items[0];
+    var items = JSON.parse(nasaBody).collection.items;
+    if (items.length === 0) {
+      return res.status(200).json(botPayload);
+    }
+    var item = items[0];
     var title = item.data[0].title;
     var description = item.data[0].description;
-    var nasaImage = item.links.href;
+    var nasaImage = item.links[0].href;
 
     botPayload = {
       response_type: 'in_channel',
@@ -37,7 +41,7 @@ module.exports = function (req, res, next) {
       mrkdwn: true,
       attachments: [
         {
-          image_url: 'https://i.kinja-img.com/gawker-media/image/upload/s--VR6Tpx6J--/c_scale,fl_progressive,q_80,w_800/18r46ljz876m2jpg.jpg',
+          image_url: nasaImage,
         },
         {
           text: description,
