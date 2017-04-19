889bafacc51385f54f68359aaad72365cafb422a
Thomas Tarazevits <ttarazevits@hubspot.com>
2017-04-18 20:33:59 -0400

    resolved roll issue

--- handlers/slash/yuri.js
+++ handlers/slash/yuri.js
@@ -10,7 +10,7 @@ module.exports = function (req, res, next) {
 
   var botPayload = {
     response_type: 'in_channel',
-    text: `You got ${roll}, dude!`,
+    text: `Here is an issue, dude!`,
     attachments: [
       {
         image_url: 'https://i.kinja-img.com/gawker-media/image/upload/s--VR6Tpx6J--/c_scale,fl_progressive,q_80,w_800/18r46ljz876m2jpg.jpg',
