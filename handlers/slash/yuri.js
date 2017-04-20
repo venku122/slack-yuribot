8f130c5f06b2aef88ded11276e63b0d61e08e180
T.J. Tarazevits <tarazevits@gmail.com>
2017-04-19 22:31:34 -0400

    Resolved mutable object issue

--- handlers/slash/yuri.js
+++ handlers/slash/yuri.js
@@ -3,15 +3,15 @@ var request = require('request');
 
 var TOKEN = process.env.SLACK_INTEGRATION_TOKEN;
 var NASAUrl = process.env.NASA_API_ROOT;
-var botPayload = {
+var notFoundPayload = Object.freeze({
   response_type: 'ephemeral',
-  text: 'You need to send a query yo',
+  text: 'There was an issue with your query',
   attachments: [
     {
       image_url: 'https://i.kinja-img.com/gawker-media/image/upload/s--VR6Tpx6J--/c_scale,fl_progressive,q_80,w_800/18r46ljz876m2jpg.jpg',
     },
   ],
-};
+});
 
 module.exports = function (req, res, next) {
   var token = req.body.token;
@@ -21,14 +21,14 @@ module.exports = function (req, res, next) {
 
   var query = req.body.text;
   if (!query) {
-    return res.status(200).json(botPayload);
+    return res.status(200).json(notFoundPayload);
   }
 
   // send query to nasa api
   request(`${NASAUrl}/search?q=${query}&media_type=image`, (err, nasaRes, nasaBody) => {
     var items = JSON.parse(nasaBody).collection.items;
     if (items.length === 0) {
-      return res.status(200).json(botPayload);
+      return res.status(200).json(notFoundPayload);
     }
     var index = Math.floor(Math.random() * items.length);
     var item = items[index];
@@ -36,7 +36,7 @@ module.exports = function (req, res, next) {
     var description = item.data[0].description;
     var nasaImage = item.links[0].href;
 
-    botPayload = {
+    var botPayload = {
       response_type: 'in_channel',
       text: `*${title}*`,
       mrkdwn: true,
